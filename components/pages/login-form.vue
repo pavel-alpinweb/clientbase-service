c64679d81ef61aacccc84ff9bd67f52f935061d4
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-04-26 16:19:18 +0600

    create login user

--- components/pages/login-form.vue
+++ components/pages/login-form.vue
@@ -1,15 +1,40 @@
 <template lang="pug">
     .common-form__content
-        form.common-form__sending-form
+        form.common-form__sending-form(@submit.prevent="onSubmit")
             .common-form__item
                 legend.common-form__title Введите данные для входа:
             .common-form__item
                 label.common-form__label Логин
-                input.common-form__input(type="text", placeholder="Введите имя пользователя")
+                input.common-form__input(v-model.trim="login", type="text", placeholder="Введите имя пользователя")
             .common-form__item
                 label.common-form__label Пароль
-                input.common-form__input(type="text", placeholder="Введите пароль")
+                input.common-form__input(v-model.trim="password", type="password", placeholder="Введите пароль")
             button.common-form__submit Войти
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      login: '',
+      password: ''
+    }
+  },
+  methods: {
+    async onSubmit () {
+      try {
+        const formData = {
+          login: this.login,
+          password: this.password
+        }
+        await this.$store.dispatch('auth/login', formData)
+        this.$router.push('/')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped src="@/assets/styles/components/form.scss"></style>

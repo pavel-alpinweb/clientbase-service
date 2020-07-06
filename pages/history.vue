b01387be7c32314869376cae961c76a8ded62298
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-07-06 11:53:02 +0600

    create search on history page

--- pages/history.vue
+++ pages/history.vue
@@ -2,7 +2,7 @@
   .history
     .history__search
       Search
-    .history__item(v-for="(client, index) in clients", :key="client.id")
+    .history__item(v-for="(client, index) in clientsArray", :key="client._id")
       HistoryClientCard(:client="client", :index="index")
 </template>
 
@@ -17,12 +17,25 @@ export default {
   },
   data () {
     return {
+      searchString: '',
       isHint: true,
       textPage:
       `На этой странице показана вся история сотрудничества со всеми клиентами.
        Введите в поиск имя или id клиента, чтобы увидеть историю сотрудничества только с данным клиентом.
        Важный совет. Прежде чем перемещать клиента из одной категории в другую, оставляйте запись почему вы это сделали и не забывайте сохранить.
        Это поможет, Вам увидеть точную ретроспективу Вашего сотрудничества.`
+    }
+  },
+  computed: {
+    clientsArray () {
+      if (this.searchString === '') {
+        return this.clients
+      } else {
+        const checkClients = this.clients.filter((client) => {
+          return client.name.toLowerCase().includes(this.searchString.toLowerCase())
+        })
+        return checkClients
+      }
     }
   },
   async asyncData ({ store }) {
@@ -33,6 +46,9 @@ export default {
   mounted () {
     this.$EventBus.$on('switchHint', (data) => {
       this.isHint = data.active
+    })
+    this.$EventBus.$on('search', (data) => {
+      this.searchString = data.searchString
     })
     this.$EventBus.$emit('changePageText', { textPage: this.textPage })
   }

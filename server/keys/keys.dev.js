373e566e801ef680f08eb9230f3257dd4d247d9b
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-09-02 09:41:20 +0600

    dev key

---
+++ server/keys/keys.dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGO_URI: `mongodb://localhost:27017/clientbase`,
+  JWT: 'clientsbase-jwt-key',
+  s3AccessKeyId: 'AKIAJZUSX4FPV6BCWOPQ',
+  s3SecretAccessKey: '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'
+}

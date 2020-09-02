0d0b13dccf8676f94d31db2cf0879ad1b85f09f0
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-09-02 10:24:35 +0600

    create rating-client component

--- pages/rating.vue
+++ pages/rating.vue
@@ -3,21 +3,30 @@
     .rating_search
       Search
     ul.rating__list
-
+      li.rating__item(v-for="(client, index) in clients")
+        .rating__position {{ index + 1 }}
+        RatingClient(:client="client", :procent="100")
 </template>
 
 <script>
 import Search from '@/components/pages/search'
+import RatingClient from '@/components/pages/rating-client'
 export default {
   middleware: ['admin-auth'],
   components: {
-    Search
+    Search,
+    RatingClient
   },
   data () {
     return {
       isHint: true,
       textPage: 'На этой странице показан рейтинг всех клиентов, а так-же процент от общей прибыли, которую принес Вам каждый из клиентов.'
     }
+  },
+  async asyncData ({ store }) {
+    const user = store.getters['auth/user']
+    const clients = await store.dispatch('client/getAll', user.userId)
+    return { clients }
   },
   mounted () {
     this.$EventBus.$on('switchHint', (data) => {
@@ -43,5 +52,24 @@ export default {
     min-height: 90vh;
     padding: 0;
     margin: 0;
+  }
+  .rating__item{
+    margin-bottom: 50px;
+    display: flex;
+    width:100%;
+    align-items: center;
+    padding-left: 20px;
+  }
+  .rating__position{
+    color: $mainColor;
+    font-family: $mainFont;
+    font-size: $h3fontSize;
+    font-weight: 700;
+  }
+  .rating__menu-item--active{
+    .button--text{
+      color: $mainColor;
+      background: transparent;
+    }
   }
 </style>

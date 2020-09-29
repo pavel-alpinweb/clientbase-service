1cef2fff6c0c87e168c23f07194151b1e1d6cffe
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-09-29 10:59:21 +0600

    display rating category with data

---
+++ components/pages/rating-category.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+.progress__item
+  .progress__procent(v-if="categoryProcent > 0") {{ categoryProcent }}%
+  .progress__bar(:style="'height:' +  categoryProcent + '%;'") {{ categorySumm }}
+  .progress__name {{ categoryName }}
+</template>
+
+<script>
+export default {
+  props: {
+    categoryTrades: {
+      type: Array,
+      required: true
+    },
+    procent: {
+      type: Number,
+      required: true
+    },
+    typeProgress: {
+      type: String,
+      required: true
+    },
+    categoryName: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    categorySumm () {
+      let summ = 0
+      if (this.typeProgress === 'payloads') {
+        for (const trade of this.categoryTrades) {
+          summ += trade.pay
+        }
+      } else if (this.typeProgress === 'trades') {
+        summ = this.categoryTrades.length
+      }
+      return summ
+    },
+    categoryProcent () {
+      let procent = 0
+      procent = (this.categorySumm / this.procent).toFixed(2)
+      if (isNaN(procent)) {
+        return 0
+      } else {
+        return procent
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/styles/layout/vars.scss';
+  .progress__item{
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      margin-bottom: -24px;
+  }
+  .progress__bar{
+      background-color: $accentColor;
+      display: flex;
+      justify-content: center;
+      align-items: flex-end;
+      color: $mainColor;
+      font-family: $mainFont;
+      font-size: $h3fontSize;
+      font-weight: 700;
+      padding-bottom: 5px;
+      width: 100%;
+  }
+  .progress__name{
+      width: 100%;
+      text-align: center;
+      color: $mainColor;
+      font-family: $mainFont;
+      font-size: $h6fontSize;
+      font-weight: 700;
+  }
+  .progress__procent{
+      width: 100%;
+      text-align: center;
+      font-size: $h5fontSize;
+      font-weight: 700;
+  }
+</style>
--- pages/progress.vue
+++ pages/progress.vue
@@ -1,19 +1,40 @@
 <template lang="pug">
   .progress
     .progress__graffics
-      pre {{ trades }}
+      ratingCategory(:categoryName="'Архивные'", :procent="procent", :typeProgress="typeProgress", :categoryTrades="getCategoryTrades('archive')")
+      ratingCategory(:categoryName="'Спящие'", :procent="procent", :typeProgress="typeProgress", :categoryTrades="getCategoryTrades('sleep')")
+      ratingCategory(:categoryName="'Открытые'", :procent="procent", :typeProgress="typeProgress", :categoryTrades="getCategoryTrades('open')")
+      ratingCategory(:categoryName="'Постоянные'", :procent="procent", :typeProgress="typeProgress", :categoryTrades="getCategoryTrades('repeat')")
+      ratingCategory(:categoryName="'V.I.P.'", :procent="procent", :typeProgress="typeProgress", :categoryTrades="getCategoryTrades('vip')")
 </template>
 
 <script>
+import ratingCategory from '@/components/pages/rating-category'
+
 export default {
   middleware: ['admin-auth'],
+  components: {
+    ratingCategory
+  },
   data () {
     return {
       isHint: true,
+      typeProgress: 'payloads',
       textPage:
       `На этой странице показана общая статистика за все время использования этого приложения. 
       Здесь вы можете узнать, сколько клиентов попадало в каждую из категорий. 
       Чтобы узнать подобно, о том кто эти клиенты, зайдите на страницу "Списки"`
+    }
+  },
+  computed: {
+    procent () {
+      let procent = 0
+      if (this.typeProgress === 'payloads') {
+        procent = this.getOnePayloadsProcent()
+      } else if (this.typeProgress === 'trades') {
+        procent = this.getOneTradesProcent()
+      }
+      return procent
     }
   },
   async asyncData ({ store }) {
@@ -26,6 +47,30 @@ export default {
       this.isHint = data.active
     })
     this.$EventBus.$emit('changePageText', { textPage: this.textPage })
+  },
+  methods: {
+    getOnePayloadsProcent () {
+      let allMoney = 0
+      let procent = 0
+      for (const trade of this.trades) {
+        allMoney += trade.pay
+      }
+      if (allMoney !== 0) {
+        procent = allMoney / 100
+      }
+      return procent
+    },
+    getOneTradesProcent () {
+      const allTrades = this.trades.length
+      let procent = 0
+      if (allTrades !== 0) {
+        procent = allTrades / 100
+      }
+      return procent
+    },
+    getCategoryTrades (category) {
+      return this.trades.filter(trade => trade.client.status === category)
+    }
   }
 }
 </script>
